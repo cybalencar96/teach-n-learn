7545bc685f49609e0ce3f12d85421fc846768c12
Carlos Alencar <iuryalencar@poli.ufrj.br>
2021-08-25 14:54:20 -0300

    ajustando headers com middleware cors

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser');
 const { raw } = require('body-parser');
 const mongo = require('./models/dbConnection');
 var mongoose = require("mongoose");
-
+const cors = require("cors")
 
 const app = express();
 const port = process.env.PORT || 5050;
 const apiVersion = "v0";
+
+app.use(cors());
 
 //Body parsers
 app.use(bodyParser.raw());
@@ -19,26 +21,6 @@ app.use(`/api/${apiVersion}`, require(`./routes/api/${apiVersion}/index.js`));
 
 //DB connect
 //mongo.connectDB();
-
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', "*");
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
-    next();
-});
 
 //Server listener
 app.listen(port, () => {
